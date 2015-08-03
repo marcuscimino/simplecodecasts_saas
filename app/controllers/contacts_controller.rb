eeb12da2d4cedbed780bc66ce20620a3122e97c5
Marcus Cimino <marcus.cimino@marcuscimino.com>
2015-08-03 01:20:06 +0000

    corrections

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,9 +11,9 @@ class ContactsController < ApplicationController
         
         if @contact.save
             
-            name = params [:contact][:name]
-            email = params [:contact][:name]
-            body = params [:contact][:comments]
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
             
             ContactMailer.contact_email(name, email, body).deliver
             

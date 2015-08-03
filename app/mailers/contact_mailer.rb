868f00af91c5bdedb0c2b9aff61c34e819a621e8
Marcus Cimino <marcus.cimino@marcuscimino.com>
2015-08-03 11:44:14 +0000

    added devise gem

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,10 +3,12 @@ class ContactMailer < ActionMailer::Base
     default to: 'marcus.cimino@marcuscimino.com'
     
     def contact_email(name, email, body)
+        
         @name = name
         @email = email
         @body = body
         
-        mail(from: email, subject: 'Contact From Message')
+        mail(from: email, subject: 'Contact Form Message')
+        
     end
 end
